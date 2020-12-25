42ae0fcc99f3f75feddbe9803481a133fe127964
Alexander Ryazanov <alexander.ryazanov@abbyy.com>
2020-12-25 15:32:03 +0300

    base

---
+++ src/Receiver/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+class Receiver
+{
+    public static async Task<int> Main()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = "r1-r-rabbitmq.exp",
+            UserName = "user",
+            Password = "password"
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+            while (true)
+            {
+                await Task.Delay(10);
+            }
+        }
+    }
+}
